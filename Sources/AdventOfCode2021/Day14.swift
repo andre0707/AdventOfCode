e53174a0b4924b7bd68293ca6b238bdb9f98088d
Andre <andre@aaindustries.de>
2021-12-15 11:43:53 +0100

    - day 14 task 2

--- Sources/AdventOfCode2021/Day14.swift
+++ Sources/AdventOfCode2021/Day14.swift
@@ -2,13 +2,14 @@ import Foundation
 
 struct Day14 {
     static func run() {
-        let input = try! String(contentsOfFile: Bundle.module.path(forResource: "Resources/input_DayTest", ofType: "txt")!)
+        let input = try! String(contentsOfFile: Bundle.module.path(forResource: "Resources/input_Day14", ofType: "txt")!)
             .components(separatedBy: "\n\n")
 
+        /// Get the starting polymere
         let polymere = input[0]
 
+        /// Fill the pair instruction dictionary
         var pairInsertionInstructions = [String: String]()
-        
         input[1]
             .components(separatedBy: .newlines)
             .forEach { instruction in
@@ -18,6 +19,11 @@ struct Day14 {
             }
         
         
+        // MARK: - Task 1
+        
+        
+        /// Idea: build the whole result string by inserting the string for each pair we find.
+        /// Loop over the current result string `step` times.
         var result = polymere
         
         func run(steps: Int, printSteps: Bool = false) {
@@ -43,9 +49,6 @@ struct Day14 {
             }
         }
         
-        
-        // MARK: - Task 1
-        
         run(steps: 10)
         var charCounter = [Character : Int]()
         for char in result {
@@ -56,8 +59,45 @@ struct Day14 {
         
         // MARK: - Task 2
         
+        /// Task 1 solution is way too slow and needs way too much memory for a huge amount of steps.
+        /// New idea: just calculate the pairs and keep count of them. Inserting a string in a pair will result in 2 new pairs.
+        /// Example: Insertion rule AB -> C will turn the pair [AB] into the following two pairs [AC, CB]
         
+        func run2(steps: Int) -> Int {
+            var currentPairsCounter = [String : Int]()
+            /// Count the pairs in the starting polymore string
+            var lastChar: Character? = nil
+            for char in polymere {
+                if lastChar == nil {
+                    lastChar = char
+                    continue
+                }
+                let pair = String([lastChar!, char])
+                currentPairsCounter[pair, default: 0] += 1
+                lastChar = char
+            }
+            
+            /// Repeat steps times the calculation for the count of pairs
+            for _ in 1 ... steps {
+                /// Calculate the next pairs from the current pairs
+                var nextPairsCounter = [String : Int]()
+                for pair in currentPairsCounter.keys {
+                    nextPairsCounter[String(pair[pair.startIndex]) + pairInsertionInstructions[pair]!, default: 0] += currentPairsCounter[pair]!
+                    nextPairsCounter[pairInsertionInstructions[pair]! + String(pair[pair.index(before: pair.endIndex)]), default: 0] += currentPairsCounter[pair]!
+                }
+                currentPairsCounter = nextPairsCounter
+            }
+            
+            /// Check how many of each characters we have. All (but the last character from the starting polymore) will be at first at a first position of the pairs
+            var characterCount = [Character : Int]()
+            characterCount[polymere[polymere.index(before: polymere.endIndex)]] = 1
+            for pair in currentPairsCounter.keys {
+                characterCount[pair[pair.startIndex], default: 0] += currentPairsCounter[pair]!
+            }
+            
+            return characterCount.values.max()! - characterCount.values.min()!
+        }
         
-        
+        print("Result task 2: \(run2(steps: 40))")
     }
 }
